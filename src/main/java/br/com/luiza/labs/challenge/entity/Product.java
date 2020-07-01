d1d2af989237fbce2c38e528fed0fe61995aee3c
Gabriel Panassol <gpanassol@gmail.com>
2020-07-01 00:21:58 -0300

    Realizado implementação do banco de dados e iniciando testes unitarios para validar insercao de produtos

---
+++ src/main/java/br/com/luiza/labs/challenge/entity/Product.java
@@ -0,0 +1,45 @@
+package br.com.luiza.labs.challenge.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="price")
+    private BigDecimal price;
+
+    @Column(name="url")
+    private String url;
+
+    @Column(name="marca")
+    private String marca;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="review_score")
+    private Double reviewScore;
+
+    public Product(){}
+
+    public Product(Integer id, BigDecimal price, String url, String marca, String title, Double reviewScore) {
+        this.id = id;
+        this.price = price;
+        this.url = url;
+        this.marca = marca;
+        this.title = title;
+        this.reviewScore = reviewScore;
+    }
+}
---
+++ src/main/java/br/com/luiza/labs/challenge/service/impl/ProductServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.luiza.labs.challenge.service.impl;
+
+import br.com.luiza.labs.challenge.entity.Product;
+import br.com.luiza.labs.challenge.repository.Products;
+import br.com.luiza.labs.challenge.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private Products repository;
+
+    @Override
+    public Optional<Product> findProductById(Integer id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Product salve(Product product) {
+        return repository.save(product);
+    }
+}
